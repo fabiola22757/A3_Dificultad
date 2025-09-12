65ea38bd553da258ac02a209a215bc4bc1d92b69
Fabiola <fabiola.juarez>
2025-09-12 17:46:41 -0600

    Initial Commit

---
+++ A3_Dificultad/A3_DificultadApp.swift
@@ -0,0 +1,17 @@
+//
+//  A3_DificultadApp.swift
+//  A3_Dificultad
+//
+//  Created by win603 on 12/09/25.
+//
+
+import SwiftUI
+
+@main
+struct A3_DificultadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A3_Dificultad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A3_Dificultad
+//
+//  Created by win603 on 12/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
